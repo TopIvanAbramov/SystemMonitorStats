64f168d3fe4313aba4526cf6e8885ee114b26c6b
TopIvanAbramov <I.abramov@innopolis.university>
2021-03-15 23:32:26 +0300

    Initial project setup

---
+++ SystemMonitorStatsExamples/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SystemMonitorStatsExamples
+//
+//  Created by Иван Абрамов on 15.03.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
