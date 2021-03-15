8f6b82248fc8dfd2d70773488a60b64c7169ed1f
TopIvanAbramov <I.abramov@innopolis.university>
2021-03-15 23:27:10 +0300

    Initial Commit

---
+++ SystemMonitorStats/SystemMonitorStats.h
@@ -0,0 +1,18 @@
+//
+//  SystemMonitorStats.h
+//  SystemMonitorStats
+//
+//  Created by Иван Абрамов on 15.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SystemMonitorStats.
+FOUNDATION_EXPORT double SystemMonitorStatsVersionNumber;
+
+//! Project version string for SystemMonitorStats.
+FOUNDATION_EXPORT const unsigned char SystemMonitorStatsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SystemMonitorStats/PublicHeader.h>
+
+
